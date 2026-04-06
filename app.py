e8b649e28c67f9aab923319e1a01d27e719372e0
Kamal AK <kamalak1506@gmail.com>
2026-04-06 16:04:06 +0530

    app.py updated

--- app.py
+++ app.py
@@ -168,6 +168,10 @@ def create_app():
     login_manager.init_app(app)
     login_manager.login_view = 'login'
 
+    with app.app_context():
+        db.create_all()
+        print("Database tables ensured")
+
 
     @app.teardown_appcontext
     def shutdown_session(exception=None):
